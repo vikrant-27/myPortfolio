3cd792cf8b58ed6725cca76d41bd36be7f68c209
vikrant-27 <vikrantsarade2002@gmail.com>
2024-08-12 20:05:24 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+import {FaSquareXTwitter} from "react-icons/fa6";
+import {FaInstagram} from "react-icons/fa";
+
+export const Navbar = () => {
+  return (
+
+    <>
+    <nav className=' mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center logoText'>
+            Vr
+        </div>
+        <div className='m-8 flex items-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaSquareXTwitter/>
+        <FaInstagram/>
+        </div>
+    </nav>
+    </>
+    
+  )
+}
+
+
+// import React from 'react'
+
+// function Navbar() {
+//   return (
+//     <div>Navbar</div>
+//   )
+// }
+
+// export default Navbar
