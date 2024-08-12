a0bc4fff43591bc80a83d26cfdda19256cddacde
vikrant-27 <vikrantsarade2002@gmail.com>
2024-08-13 00:35:51 +0530

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {HERO_CONTENT} from '../constants'
+import profilepic from '../assets/vkProfile.png'
 
 export const Hero = () => {
   return (
@@ -10,7 +12,13 @@ export const Hero = () => {
                 <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
                     Full Stack Developer
                 </span>
+                <p className='my-2 max-w-xl py-6 font-light tracking-tight'>{HERO_CONTENT}</p>
             </div>            
+            </div>
+            <div className='w-full lg:w-1/2  lg:p-8'>
+               <div className='ml-24' >
+                <img className='imgprop rounded-box' src={profilepic} alt="profilepic" />
+               </div>
             </div>
         </div>
        
