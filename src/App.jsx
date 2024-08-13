106ae2d69417f4a0bb9176ff4f0ac10ce38f767a
vikrant-27 <vikrantsarade2002@gmail.com>
2024-08-13 11:39:47 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
+import { About } from './components/About'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <div className='container mx-auto px-12'>
     <Navbar/> 
     <Hero/>
+    <About/>
     </div>
 
  
