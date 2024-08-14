19aeb9f72300ec1eee0ec0d203126f87daa9926d
vikrant-27 <vikrantsarade2002@gmail.com>
2024-08-15 00:18:19 +0530

    minor changer

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,9 +9,9 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    year: "2024 - Present",
+    role: "Java Full Stack Developer Intern",
+    company: "Pisyst.",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
@@ -29,48 +29,63 @@ export const EXPERIENCES = [
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Skull Candy",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A website for purchasing headphones. This project includes features such as product listings, detailed product pages, a shopping cart, and secure checkout.",
+    technologies: ["HTML", "CSS", "Python", "Django", "SQLlite", "Bootstrap"],
   },
   {
-    title: "Task Management App",
+    title: "Resume Generator",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A CV generator application where users can create and download their resumes",
+    technologies: ["HTML", "CSS","Bootstrap", "Javascript", "MongoDb" , "Node Js " ,"Express Js" ],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "Tailwind Css" , "React Js" ],
   },
   {
-    title: "Blogging Platform",
+    title: "Bookish",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Developed a comprehensive Bookstore App, a web application that enables users to browse books seamlessly. This project leveraged the MERN stack",
+    technologies: ["HTML", "Tailwind Css", "React Js", "Node Js", "Express Js", "MongoDb"],
+  },
+  {
+    title: "EMS",
+    image: project3,
+    description:
+      "Developed an Employee Management System using Python with Django framework. Utilized CRUD operations to efficiently manage employee data, allowing for easy updating and manipulation of records.",
+    technologies: ["HTML", "Css", "Bootstrap", "Python", "Django", "mySql"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Javascript", ],
+  },
+  {
+    title: "Temperature Converter",
+    image: project1,
+    description:
+      "A temperature converter web application built with JavaScript that allows users to convert temperatures between Celsius, Fahrenheit, and Kelvin.",
+    technologies: ["HTML", "CSS", "Javascript", ],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "A-301 ,  P Town   Maharashtra , IN - 264 ",
+  phoneNo: "+91 9503194686 ",
+  email: "vikrant45.exe@gmail.com",
 };
